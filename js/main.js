4604e66eec37e7b69c47b4af4fce856b7951544e
Andres Atencio <and.atencio@gmail.com>
2014-12-11 08:47:14 -0300

    minify

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ $(function () {
 	function tomarDatos (cb) {
 
 		var heatMap = [];
-		$.getJSON('data/r1.json', function (reclamos) {
+		$.getJSON('data/r1.min.json', function (reclamos) {
 			$('body').loadie(0.7);
 			$("#r").html(reclamos.length)
 			for (var i = 0; i < reclamos.length; i++) {
